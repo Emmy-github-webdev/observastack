2521070ea4177702f751b242a1a8ef85f47700ea
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-09-03 21:55:42 +0200

    Added public, private, and database subnets

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -27,3 +27,72 @@ resource "aws_internet_gateway" "observastack_igw" {
     }
   )
 }
+
+###########################
+# Public Subnets
+###########################
+resource "aws_subnet" "pub_subnet" {
+  for_each = {
+    for index, az in var.availability_zones :
+    az => index
+  }
+
+  vpc_id                  = aws_vpc.observastack_vpc.id
+  availability_zone       = each.key
+  cidr_block              = cidrsubnet(var.vpc_cidr, 4, each.value)
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name                     = "${local.name_prefix}-public-${each.key}"
+      Tier                     = "public"
+      "kubernetes.io/role/elb" = "1"
+    }
+  )
+}
+
+############################
+# Private application subnets
+############################
+resource "aws_subnet" "priv_subnet" {
+  for_each = {
+    for index, az in var.availability_zones :
+    az => index
+  }
+
+  vpc_id            = aws_vpc.observastack_vpc.id
+  availability_zone = each.key
+  cidr_block        = cidrsubnet(var.vpc_cidr, 4, each.value + 4)
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name                              = "${local.name_prefix}-private-${each.key}"
+      Tier                              = "private"
+      "kubernetes.io/role/internal-elb" = "1"
+    }
+  )
+}
+
+#############################
+# Database subnets
+#############################
+resource "aws_subnet" "database" {
+  for_each = {
+    for index, az in var.availability_zones :
+    az => index
+  }
+
+  vpc_id            = aws_vpc.observastack_vpc.id
+  availability_zone = each.key
+  cidr_block        = cidrsubnet(var.vpc_cidr, 4, each.value + 8)
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${local.name_prefix}-database-${each.key}"
+      Tier = "database"
+    }
+  )
+}
