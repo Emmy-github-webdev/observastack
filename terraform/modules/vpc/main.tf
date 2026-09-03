2d376d7dad6f577fa44abb9ee08fe0eb9563aa3a
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-09-03 22:10:23 +0200

    Added VPC flow logs

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -235,3 +235,15 @@ resource "aws_eip" "eip_nat" {
     }
   )
 }
+
+##############################
+# VPC Flow Logs
+##############################
+resource "aws_cloudwatch_log_group" "flow_logs" {
+  count = var.enable_flow_logs ? 1 : 0
+
+  name              = "/aws/vpc/${local.name_prefix}/flow-logs"
+  retention_in_days = var.flow_logs_retention_days
+
+  tags = local.common_tags
+}
