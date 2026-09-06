829b196fb87b3156a55feaeb4ff2c2a10df3b708
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-09-06 19:10:16 +0200

    Added EKS node IAM role

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -41,3 +41,39 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   role       = aws_iam_role.eks_cluster[0].name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
+
+###############################
+# EKS Node IAM Role 
+###############################
+data "aws_iam_policy_document" "eks_node_assume_role" {
+  statement {
+    sid    = "AllowEC2ServiceAssumeRole"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = [
+      "sts:AssumeRole"
+    ]
+  }
+}
+
+resource "aws_iam_role" "eks_node" {
+  count = var.create_eks_node_role ? 1 : 0
+
+  name = "${local.name_prefix}-eks-node-role"
+
+  assume_role_policy = data.aws_iam_policy_document.eks_node_assume_role.json
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name      = "${local.name_prefix}-eks-node-role"
+      Component = "eks"
+      Purpose   = "worker-node"
+    }
+  )
+}
