c4b67bb14332beaa907bf0476fe94aa8b7bbd068
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-09-06 19:16:41 +0200

    Load Balancer Controller IAM Role

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -77,3 +77,67 @@ resource "aws_iam_role" "eks_node" {
     }
   )
 }
+
+##########################################
+# Attach the standard EKS node permissions
+##########################################
+
+resource "aws_iam_role_policy_attachment" "eks_node_worker" {
+  count = var.create_eks_node_role ? 1 : 0
+
+  role       = aws_iam_role.eks_node[0].name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_ecr" {
+  count = var.create_eks_node_role ? 1 : 0
+
+  role       = aws_iam_role.eks_node[0].name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_cni" {
+  count = var.create_eks_node_role ? 1 : 0
+
+  role       = aws_iam_role.eks_node[0].name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+#######################################
+# Load Balancer Controller IAM Role
+#######################################
+data "aws_iam_policy_document" "load_balancer_assume_role" {
+  count = var.create_load_balancer_role ? 1 : 0
+
+  statement {
+    sid    = "AllowEKSPodIdentity"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["pods.eks.amazonaws.com"]
+    }
+
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession"
+    ]
+  }
+}
+
+resource "aws_iam_role" "load_balancer" {
+  count = var.create_load_balancer_role ? 1 : 0
+
+  name = "${local.name_prefix}-aws-load-balancer-controller"
+
+  assume_role_policy = data.aws_iam_policy_document.load_balancer_assume_role[0].json
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name      = "${local.name_prefix}-aws-load-balancer-controller"
+      Component = "eks"
+      Purpose   = "aws-load-balancer-controller"
+    }
+  )
+}
