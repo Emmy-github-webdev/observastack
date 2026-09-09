f58abc239de4b2bbc5d96448baaa2a2e9c6c3ad0
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-09-09 16:38:33 +0200

    Troubleshoot workflow exits

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -18,99 +18,99 @@ module "vpc" {
   tags = local.common_tags
 }
 
-module "kms" {
-  source = "../../modules/kms"
+# module "kms" {
+#   source = "../../modules/kms"
 
-  project_name = "observastack"
-  environment  = "dev"
+#   project_name = "observastack"
+#   environment  = "dev"
 
-  # deletion_window_in_days = 7
-  deletion_window_in_days = 0 # for testing purposes only
+#   # deletion_window_in_days = 7
+#   deletion_window_in_days = 0 # for testing purposes only
 
-  tags = local.common_tags
-}
+#   tags = local.common_tags
+# }
 
-module "iam" {
-  source = "../../modules/iam"
+# module "iam" {
+#   source = "../../modules/iam"
 
-  project_name = "observastack"
-  environment  = "dev"
+#   project_name = "observastack"
+#   environment  = "dev"
 
-  # create_eks_cluster_role     = true
-  # create_eks_node_role        = true
-  # create_load_balancer_role   = true
-  # create_external_secrets_role = true
-  # create_application_roles    = true
+#   # create_eks_cluster_role     = true
+#   # create_eks_node_role        = true
+#   # create_load_balancer_role   = true
+#   # create_external_secrets_role = true
+#   # create_application_roles    = true
 
-  application_names = [
-    "user-service",
-    "product-service",
-    "order-service",
-    "payment-service"
-  ]
+#   application_names = [
+#     "user-service",
+#     "product-service",
+#     "order-service",
+#     "payment-service"
+#   ]
 
-  tags = local.common_tags
-}
+#   tags = local.common_tags
+# }
 
-module "eks" {
-  source = "../../modules/eks"
+# module "eks" {
+#   source = "../../modules/eks"
 
-  project_name = "observastack"
-  environment  = "dev"
+#   project_name = "observastack"
+#   environment  = "dev"
 
-  kubernetes_version = "1.35"
+#   kubernetes_version = "1.35"
 
-  vpc_id = module.vpc.vpc_id
+#   vpc_id = module.vpc.vpc_id
 
-  private_subnet_ids = module.vpc.private_subnet_ids
+#   private_subnet_ids = module.vpc.private_subnet_ids
 
-  cluster_role_arn = module.iam.eks_cluster_role_arn
-  node_role_arn    = module.iam.eks_node_role_arn
+#   cluster_role_arn = module.iam.eks_cluster_role_arn
+#   node_role_arn    = module.iam.eks_node_role_arn
 
-  vpc_cni_role_arn = module.iam.vpc_cni_role_arn
-  ebs_csi_role_arn = module.iam.ebs_csi_role_arn
+#   vpc_cni_role_arn = module.iam.vpc_cni_role_arn
+#   ebs_csi_role_arn = module.iam.ebs_csi_role_arn
 
-  cluster_endpoint_private_access = true
-  cluster_endpoint_public_access  = false
+#   cluster_endpoint_private_access = true
+#   cluster_endpoint_public_access  = false
 
-  cluster_encryption_kms_key_arn = module.kms.key_arn
-  cluster_log_kms_key_arn        = module.kms.key_arn
+#   cluster_encryption_kms_key_arn = module.kms.key_arn
+#   cluster_log_kms_key_arn        = module.kms.key_arn
 
-  # cluster_log_retention_days = 7
-  cluster_log_retention_days = 0 # for testing purposes only
+#   # cluster_log_retention_days = 7
+#   cluster_log_retention_days = 0 # for testing purposes only
 
-  node_group_instance_types = [
-    "t3.medium"
-  ]
+#   node_group_instance_types = [
+#     "t3.medium"
+#   ]
 
-  node_group_capacity_type = "ON_DEMAND"
+#   node_group_capacity_type = "ON_DEMAND"
 
-  node_group_min_size     = 2
-  node_group_desired_size = 2
-  node_group_max_size     = 4
+#   node_group_min_size     = 2
+#   node_group_desired_size = 2
+#   node_group_max_size     = 4
 
-  node_group_disk_size = 50
+#   node_group_disk_size = 50
 
-  tags = local.common_tags
-}
+#   tags = local.common_tags
+# }
 
-module "rds" {
-  source              = "../../modules/rds"
-  vpc_id              = module.vpc.vpc_id
-  project_name        = "observastack"
-  environment         = "dev"
-  kms_key_arn         = module.kms.key_arn
-  database_subnet_ids = module.vpc.database_subnet_ids
-  tags                = local.common_tags
-}
+# module "rds" {
+#   source              = "../../modules/rds"
+#   vpc_id              = module.vpc.vpc_id
+#   project_name        = "observastack"
+#   environment         = "dev"
+#   kms_key_arn         = module.kms.key_arn
+#   database_subnet_ids = module.vpc.database_subnet_ids
+#   tags                = local.common_tags
+# }
 
-module "redis" {
-  source = "../../modules/redis"
+# module "redis" {
+#   source = "../../modules/redis"
 
-  project_name     = "observastack"
-  environment      = "dev"
-  vpc_id           = module.vpc.vpc_id
-  cache_subnet_ids = module.vpc.database_subnet_ids
-  kms_key_arn      = module.kms.key_arn
-  tags             = local.common_tags
-}
+#   project_name     = "observastack"
+#   environment      = "dev"
+#   vpc_id           = module.vpc.vpc_id
+#   cache_subnet_ids = module.vpc.database_subnet_ids
+#   kms_key_arn      = module.kms.key_arn
+#   tags             = local.common_tags
+# }
