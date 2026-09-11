568f6eb3035ba384714755844d642de667929116
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-09-11 22:49:48 +0200

    Test global kms

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -63,12 +63,12 @@ output "secret_arns" {
   sensitive   = true
 }
 
-output "ecr_repository_name" {
-  description = "Shared ObservaStack ECR repository name."
-  value       = module.ecr.repository_name
-}
+# output "ecr_repository_name" {
+#   description = "Shared ObservaStack ECR repository name."
+#   value       = data.aws_ecr_repository.observastack.name
+# }
 
-output "ecr_repository_url" {
-  description = "Shared ObservaStack ECR repository URL."
-  value       = module.ecr.repository_url
-}
+# output "ecr_repository_url" {
+#   description = "Shared ObservaStack ECR repository URL."
+#   value       = data.aws_ecr_repository.observastack.repository_url
+# } 
--- terraform/global/kms/main.tf
+++ terraform/global/kms/main.tf
@@ -1,34 +1,34 @@
-# data "aws_iam_policy_document" "key_policy" {
-#   statement {
-#     sid    = "EnableRootAccountPermissions"
-#     effect = "Allow"
+data "aws_iam_policy_document" "key_policy" {
+  statement {
+    sid    = "EnableRootAccountPermissions"
+    effect = "Allow"
 
-#     principals {
-#       type = "AWS"
+    principals {
+      type = "AWS"
 
-#       identifiers = [
-#         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-#       ]
-#     }
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      ]
+    }
 
-#     actions = [
-#       "kms:*"
-#     ]
+    actions = [
+      "kms:*"
+    ]
 
-#     resources = ["*"]
-#   }
-# }
+    resources = ["*"]
+  }
+}
 
-# resource "aws_kms_key" "ecr" {
-#   description             = var.description
-#   enable_key_rotation     = var.enable_key_rotation
-#   deletion_window_in_days = var.deletion_window_in_days
-#   policy                  = data.aws_iam_policy_document.key_policy.json
+resource "aws_kms_key" "ecr" {
+  description             = var.description
+  enable_key_rotation     = var.enable_key_rotation
+  deletion_window_in_days = var.deletion_window_in_days
+  policy                  = data.aws_iam_policy_document.key_policy.json
 
-#   tags = local.common_tags
-# }
+  tags = local.common_tags
+}
 
-# resource "aws_kms_alias" "ecr" {
-#   name          = local.key_alias
-#   target_key_id = aws_kms_key.ecr.key_id
-# }
+resource "aws_kms_alias" "ecr" {
+  name          = local.key_alias
+  target_key_id = aws_kms_key.ecr.key_id
+}
