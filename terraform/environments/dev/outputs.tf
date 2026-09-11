d00db5ad38ba5e47431f0681148ad20919a3b36c
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-09-11 13:06:20 +0200

    Fixed output erorrs

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -47,10 +47,10 @@ output "observability_dashboard_name" {
   value       = module.observability.dashboard_name
 }
 
-output "observability_log_groups" {
-  description = "Development CloudWatch log groups."
-  value       = module.observability.log_group_arns
-}
+# output "observability_log_groups" {
+#   description = "Development CloudWatch log groups."
+#   value       = module.observability.log_group_arns
+# }
 
 output "secret_arns" {
   description = "Application secret ARNs managed by the Secrets Manager module."
