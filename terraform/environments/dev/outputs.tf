caa8a55c10eae2934a8d894356e07b067c3d0e33
emmy-github-webdev <ogahemmanuel75@gmail.com>
2026-09-11 21:40:47 +0200

    Test root dev env output

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -1,34 +1,34 @@
-# output "vpc_id" {
-#   description = "Development VPC ID."
-#   value       = module.vpc.vpc_id
-# }
+output "vpc_id" {
+  description = "Development VPC ID."
+  value       = module.vpc.vpc_id
+}
 
-# output "private_subnet_ids" {
-#   description = "Development private subnet IDs."
-#   value       = module.vpc.private_subnet_ids
-# }
+output "private_subnet_ids" {
+  description = "Development private subnet IDs."
+  value       = module.vpc.private_subnet_ids
+}
 
-# output "database_subnet_ids" {
-#   description = "Development database subnet IDs."
-#   value       = module.vpc.database_subnet_ids
-# }
+output "database_subnet_ids" {
+  description = "Development database subnet IDs."
+  value       = module.vpc.database_subnet_ids
+}
 
-# output "eks_cluster_name" {
-#   description = "Development EKS cluster name."
-#   value       = module.eks.cluster_name
-# }
+output "eks_cluster_name" {
+  description = "Development EKS cluster name."
+  value       = module.eks.cluster_name
+}
 
-# output "eks_cluster_endpoint" {
-#   description = "Development EKS Kubernetes API endpoint."
-#   value       = module.eks.cluster_endpoint
-#   sensitive   = true
-# }
+output "eks_cluster_endpoint" {
+  description = "Development EKS Kubernetes API endpoint."
+  value       = module.eks.cluster_endpoint
+  sensitive   = true
+}
 
-# output "rds_endpoint" {
-#   description = "Development PostgreSQL endpoint."
-#   value       = module.rds.endpoint
-#   sensitive   = true
-# }
+output "rds_endpoint" {
+  description = "Development PostgreSQL endpoint."
+  value       = module.rds.endpoint
+  sensitive   = true
+}
 
 # output "rds_master_user_secret_arn" {
 #   description = "AWS-managed RDS master-user secret ARN."
